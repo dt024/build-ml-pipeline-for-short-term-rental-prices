af977048f4397f0d3bb1487583999a32cfe3da37
dt024 <ductuan@Tuan.localdomain>
2022-06-29 11:29:58 +0900

    adding filtering longtitude

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -36,7 +36,6 @@ def go(args):
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
 
-    # save_path = os.path.join(args.tmp_directory, args.output_artifact)
     df.to_csv(args.output_artifact, index=False)
     logger.info("Saved processed file path: %s" , args.output_artifact)
 
